cec8b4ee80b2322c1027833d5b41e1ef10858232
GODMAN OWUSU <71272701+geep97@users.noreply.github.com>
2023-07-20 23:03:05 +0000

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
 
-apiKey: "AIzaSyAvuR5i1qhlshxgk924dvE71X1tLDpkdmo",
-  authDomain: "chatapp-54ad8.firebaseapp.com",
-  projectId: "chatapp-54ad8",
-  storageBucket: "chatapp-54ad8.appspot.com",
-  messagingSenderId: "61100152843",
-  appId: "1:61100152843:web:fd1a6922e1c369a98dcf0e",
-  measurementId: "G-GX726JERQ5"
+apiKey: ",
+  authDomain: 
+  projectId: "
+  storageBucket: ",
+  messagingSenderId:
+  appId:
+  measurementId: "
 
    }
