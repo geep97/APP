20ff2b97665092977fd94a0e6cb8847326b83315
GODMAN OWUSU <71272701+geep97@users.noreply.github.com>
2023-07-20 22:55:54 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './screens/Login';
+import Homepage from './screens/Homepage';
+import FriendList from './screens/friendlist';
+import { firebaseConfig } from './firebaseConfig'; // Import the Firebase configuration
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Homepage" component={Homepage} />
+        <Stack.Screen name="FriendList" component={FriendList} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
